23522080be8bf681f0c0361dd999c55689de0e27
web2164 <webserver2164@gmail.com>
2024-09-15 14:04:54 +0900

    백엔드 로그인 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,8 +6,9 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 
-router.get("/", ctrl.home );
+router.get("/", ctrl.output.home );
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
 
-router.get("/login", ctrl.login);
 
 module.exports = router;
