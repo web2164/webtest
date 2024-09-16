546ce56fac49dfca1cf069c462734eb14416921f
web2164 <webserver2164@gmail.com>
2024-09-16 16:05:40 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    psword = document.querySelector("#psword"),
+    confirmpPword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
+
+registerBtn.addEventListener("click",register);
+
+function register(){
+    const req = {
+        id: id.value,
+        name: name.value,
+        psword: psword.value,
+        confirmpPword: confirmpPword.value,
+    };
+    console.log(req);
+
+    // fetch("/register", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(req),
+    // })
+    //     .then((res) => res.json())
+    //     .then((res) => {
+    //         if (res.success) {
+    //             location.href = "/";
+    //     } else {
+    //         alert(res.msg);
+    //     }
+    // })
+    // .catch((err) => {
+    //     console.error("로그인 중 에러 발생");
+    // });
+}
