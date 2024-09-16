f27d060cc349e257a2a31c3c5bb7d14072619123
web2164 <webserver2164@gmail.com>
2024-09-16 17:42:42 +0900

    회원가입 라우팅

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,30 +9,31 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click",register);
 
 function register(){
+    if (!id.value) return alert("아이디를 입력해주세요.");
+    if (psword.value !== confirmpPword.value) return alert("비밀번호가 일치하지 않습니다.");
+
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmpPword: confirmpPword.value,
     };
-    console.log(req);
 
-    // fetch("/register", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(req),
-    // })
-    //     .then((res) => res.json())
-    //     .then((res) => {
-    //         if (res.success) {
-    //             location.href = "/";
-    //     } else {
-    //         alert(res.msg);
-    //     }
-    // })
-    // .catch((err) => {
-    //     console.error("로그인 중 에러 발생");
-    // });
+    fetch("/register", {   
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/login";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error("회원가입 중 에러 발생");
+    });
 }
