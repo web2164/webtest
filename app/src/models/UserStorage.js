6633dbb32d9b6637c5f4911219b5bd32bb3afd9e
web2164 <webserver2164@gmail.com>
2024-09-16 18:01:24 +0900

    루트에 src파일 복구

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class UserStorage {
+    static #users = {
+        id: ["aaa","bbb","ccc"],
+        psword: ["111","222","333"],
+        name: ["갑","을","병"],
+    };
+
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.psword.push(userInfo.psword);
+        return { success : true };
+    }
+}
+
+module.exports = UserStorage;
