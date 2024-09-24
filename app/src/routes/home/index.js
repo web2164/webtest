00b72cc3289f5c933821792f53f9ed7ae9c2e0d9
web2164 <webserver2164@gmail.com>
2024-09-24 16:11:59 +0900

    Add files via upload

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,7 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-
-router.get("/", ctrl.output.home );
+router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
 
